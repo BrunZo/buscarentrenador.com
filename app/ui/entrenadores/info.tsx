f28a8a0d131b853c66a27fdd3305b4adad0e5cc0
unknown <urieljolo@gmail.com>
2025-12-16 17:57:27 -0300

    style: Info trainer card

--- app/ui/entrenadores/info.tsx
+++ app/ui/entrenadores/info.tsx
@@ -6,14 +6,14 @@ export default function Info({ entrenador }: {
 }) {
   return (
     <div className='flex gap-8'>
-      <div className='bg-gray-200 w-36 h-36 rounded-full'>
+      {/*<div className='bg-gray-200 w-36 h-36 rounded-full'>
         <Image
           src='/images/entrenador.jpg'
           alt='Entrenador'
           width={36}
           height={36}
         />
-      </div>
+      </div>*/}
       <div className='flex-1'>
         <h2 className='text-xl font-bold'>{entrenador.name} {entrenador.surname}</h2>
         <p className='text-gray-600 mb-3'>{entrenador.city}, {entrenador.province}</p>
@@ -53,7 +53,7 @@ function renderChips(arr: boolean[] | string[] | undefined, options: string[]) {
       {activeOptions.map((option, i) => (
         <span
           key={i}
-          className='inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800'
+          className='inline-flex items-center px-2 py-0.5 rounded-full text-xs font-light bg-indigo-100 text-indigo-800'
         >
           {option}
         </span>
